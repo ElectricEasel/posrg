3a40a783d8d81f86325a6afc7671101a2258f234
Bryan McCaffrey <bryan@electriceasel.com>
2014-08-26 16:33:51 -0500

    removing 3rd slide

--- modules/mod_welcome/tmpl/default.php
+++ modules/mod_welcome/tmpl/default.php
@@ -22,7 +22,6 @@ $template_path = '/templates/posrg';
 	<div class="slides">
 		<img src="<?php echo $template_path; ?>/images/banner-new-hardware.jpg" alt="New Hardware" />
 		<a href="/about-us/posrg-news"><img src="<?php echo $template_path; ?>/images/banner-news1.jpg" alt="POSRG News" /></a>
-		<a href="/our-process/posrg-certified"><img src="<?php echo $template_path; ?>/images/banner-hardware.jpg" alt="" /></a>
 		<a href="/inventory/search-our-inventory"><img src="<?php echo $template_path; ?>/images/banner-posrg.jpg" alt="" /></a>
 		<a href="/product-info"><img src="<?php echo $template_path; ?>/images/banner-brands.jpg" alt="" /></a>
 		<a href="/our-process/green-program"><img src="<?php echo $template_path; ?>/images/banner-green.jpg" alt="" /></a>
