030c24f5583581ebbe569642e5d6875a828ad5c9
Bryan McCaffrey <bryan@electriceasel.com>
2014-09-12 14:21:12 -0500

    home slider image link, button added to img

--- modules/mod_welcome/tmpl/default.php
+++ modules/mod_welcome/tmpl/default.php
@@ -20,7 +20,7 @@ $template_path = '/templates/posrg';
 	// ]]>
 	</script>
 	<div class="slides">
-		<img src="<?php echo $template_path; ?>/images/banner-new-hardware.jpg" alt="New Hardware" />
+		<a href="/preferred-oems"><img src="<?php echo $template_path; ?>/images/banner-new-hardware.jpg" alt="New Hardware" /></a>
 		<a href="/about-us/posrg-news"><img src="<?php echo $template_path; ?>/images/banner-news1.jpg" alt="POSRG News" /></a>
 		<a href="/inventory/search-our-inventory"><img src="<?php echo $template_path; ?>/images/banner-posrg.jpg" alt="" /></a>
 		<a href="/product-info"><img src="<?php echo $template_path; ?>/images/banner-brands.jpg" alt="" /></a>
