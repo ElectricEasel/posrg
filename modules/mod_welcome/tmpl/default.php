f54c55a6a660621e8b0b943caae0170bfc9c58e5
Ricky Goldman <ricky@electriceasel.com>
2015-03-04 11:24:25 -0600

    adding contest banner

--- modules/mod_welcome/tmpl/default.php
+++ modules/mod_welcome/tmpl/default.php
@@ -20,12 +20,13 @@ $template_path = '/templates/posrg';
 	// ]]>
 	</script>
 	<div class="slides">
+		<a target="_blank" href="http://smallbusinessgrant.fedex.com/Gallery/Detail/dd62b8a0-7a3f-4031-bf92-daacda8c15ee/?cmp=EMT-1002300-6-1-200-1111110-US-US-EN-SBGCENTRYAPPR00&acxrid=1225580814&LINK=SeeYourProfile"><img src="<?php echo $template_path; ?>/images/banner-contest.jpg" alt="Vote for POSRG" /></a>
 		<a href="/preferred-oems"><img src="<?php echo $template_path; ?>/images/banner-new-hardware.jpg" alt="New Hardware" /></a>
 		<a href="/about-us/posrg-news"><img src="<?php echo $template_path; ?>/images/banner-news1.jpg" alt="POSRG News" /></a>
-		<a href="/inventory/search-our-inventory"><img src="<?php echo $template_path; ?>/images/banner-posrg.jpg" alt="" /></a>
-		<a href="/product-info"><img src="<?php echo $template_path; ?>/images/banner-brands.jpg" alt="" /></a>
-		<a href="/our-process/green-program"><img src="<?php echo $template_path; ?>/images/banner-green.jpg" alt="" /></a>
-		<a href="/our-process/quality-assurance"><img src="<?php echo $template_path; ?>/images/banner-advantage3.jpg" alt="" /></a>
+		<a href="/inventory/search-our-inventory"><img src="<?php echo $template_path; ?>/images/banner-posrg.jpg" alt="Streamlined, organized and precise. That's POSRG." /></a>
+		<a href="/product-info"><img src="<?php echo $template_path; ?>/images/banner-brands.jpg" alt="All major brands - no major headaches." /></a>
+		<a href="/our-process/green-program"><img src="<?php echo $template_path; ?>/images/banner-green.jpg" alt="POSRG - Where &ldquo;green&rdquo; is a lifestyle." /></a>
+		<a href="/our-process/quality-assurance"><img src="<?php echo $template_path; ?>/images/banner-advantage3.jpg" alt="Yes: real people actually pick up the phone." /></a>
 	</div>
 	<div class="slidenav"></div>
 </div>
