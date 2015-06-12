49e33daea9bab09e27333f1320fb1c66397e4b4f
Ricky Goldman <ricky@electriceasel.com>
2015-06-12 09:29:05 -0500

    adding google+ publisher to head

--- templates/posrg/index.php
+++ templates/posrg/index.php
@@ -55,6 +55,7 @@ $pageclass = $menu_active->params->get('pageclass_sfx');
 	<meta name="DC.language" content="en-US" scheme="rfc1766" />
 	<meta name="DC.subject" content="Point of Sale Refurbishing" />
     <link href='http://fonts.googleapis.com/css?family=Droid+Serif' rel='stylesheet' type='text/css' />
+    <link href="https://plus.google.com/u/0/115660277677228108714" rel="publisher" />
 	<script type="text/javascript">
 	// <![CDATA[
 		var _gaq = _gaq || [];
