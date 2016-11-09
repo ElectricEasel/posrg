c1f03191b3a1000f6004669ff9d0f860c93b6663
Ricky Goldman <ricky@electriceasel.com>
2016-11-09 10:31:22 -0600

    ftp csv to server script

--- leads/leadscron.php
+++ leads/leadscron.php
@@ -1,2 +1,18 @@
 <?php
-die('hi');
+
+$dt = new DateTime("now", new DateTimeZone('America/Chicago'));
+$dt->setTimestamp(time());
+$time = $dt->format('Ymd_Hi');
+
+$ftp_server		= 'Neptune.posrg.com';
+$ftp_user		= '.\ftpuser';
+$ftp_password	= '$1lentN1ght!';
+$ftp_port		= 44021;
+
+$local_file		= getcwd().'/WebsiteLeadsCombined.csv';
+$server_file	= 'ftp://'.$ftp_user.':'.$ftp_password.'@'.$ftp_server.':'.$ftp_port.'/WebsiteLeadsCombined_'.$time.'.csv';
+
+$local_contents = file_get_contents($local_file);
+$remote = fopen($server_file,'w');
+fwrite($remote, $local_contents);
+fclose($remote);
