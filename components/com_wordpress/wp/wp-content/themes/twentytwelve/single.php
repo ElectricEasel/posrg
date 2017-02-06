96ea451511eb30cda59f134bfcca4b07df5b4497
kylemagnusson1 <kyle@electriceasel.com>
2017-02-06 12:00:07 -0600

    blog nav

--- components/com_wordpress/wp/wp-content/themes/twentytwelve/single.php
+++ components/com_wordpress/wp/wp-content/themes/twentytwelve/single.php
@@ -14,8 +14,8 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 				<nav class="nav-single">
 					<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
-					<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> BACK TO BLOG' ); ?></span>
-					<span class="nav-next"><?php next_post_link( '%link', 'NEXT ARTICLE <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
+					<span class="nav-previous"><a href="/blog">BACK TO BLOG</a></span>
+					<span class="nav-next"><?php next_post_link( '%link', 'NEXT ARTICLE <span class="meta-nav">' . _x(  ) . '</span>' ); ?></span>
 				</nav><!-- .nav-single -->
 				<?php get_template_part( 'content', get_post_format() ); ?>
 
