8be37d1bf513e19ad84eb9dde600137f12aefe3f
kylemagnusson1 <kyle@electriceasel.com>
2017-02-06 11:00:28 -0600

    blog nav

--- components/com_wordpress/wp/wp-content/themes/twentytwelve/single.php
+++ components/com_wordpress/wp/wp-content/themes/twentytwelve/single.php
@@ -12,14 +12,12 @@ get_header(); ?>
 	<div id="primary" class="site-content <?php if( !get_site_option( 'wpj_use_wp_sidebar', 1 ) ) { echo 'full'; } ?> ">
 		<div id="content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'content', get_post_format() ); ?>
-
 				<nav class="nav-single">
 					<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
-					<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
-					<span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
+					<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> BACK TO BLOG' ); ?></span>
+					<span class="nav-next"><?php next_post_link( '%link', 'NEXT ARTICLE <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
 				</nav><!-- .nav-single -->
+				<?php get_template_part( 'content', get_post_format() ); ?>
 
 				<?php comments_template( '', true ); ?>
 
