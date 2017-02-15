bd1783d23273ee7a34f9f30f2edab4d83e17429c
kylemagnusson1 <kyle@electriceasel.com>
2017-02-15 11:20:37 -0600

    remove banners

--- modules/mod_welcome/tmpl/default.php
+++ modules/mod_welcome/tmpl/default.php
@@ -24,10 +24,10 @@ $template_path = '/templates/posrg';
 		<a href="/about-us/posrg-news/168-posrg-r2-certified-for-responsible-electronics-recycling"><img src="<?php echo $template_path; ?>/images/banner-responsible-recycling.jpg" alt="POSRG Certified for Responsible Electronics Recycling. Certification positions POSRG as leader in point of sale solutions industry." /></a>
 		<a href="/about-us/posrg-news/166-posrg-naid-aaa-certified-for-data-destruction"><img src="<?php echo $template_path; ?>/images/banner-naid.jpg" alt="POSRG NAID AAA Certified for Data Destruction" /></a>
 		<a href="/preferred-oems"><img src="<?php echo $template_path; ?>/images/banner-new-hardware.jpg" alt="New Hardware" /></a>
-		<a href="/inventory/search-our-inventory"><img src="<?php echo $template_path; ?>/images/banner-posrg.jpg" alt="Streamlined, organized and precise. That's POSRG." /></a>
+<!-- 		<a href="/inventory/search-our-inventory"><img src="<?php echo $template_path; ?>/images/banner-posrg.jpg" alt="Streamlined, organized and precise. That's POSRG." /></a> -->
 		<a href="/product-info"><img src="<?php echo $template_path; ?>/images/banner-brands.jpg" alt="All major brands - no major headaches." /></a>
-		<a href="/our-process/green-program"><img src="<?php echo $template_path; ?>/images/banner-green.jpg" alt="POSRG - Where &ldquo;green&rdquo; is a lifestyle." /></a>
-		<a href="/our-process/quality-assurance"><img src="<?php echo $template_path; ?>/images/banner-advantage3.jpg" alt="Yes: real people actually pick up the phone." /></a>
+<!-- 		<a href="/our-process/green-program"><img src="<?php echo $template_path; ?>/images/banner-green.jpg" alt="POSRG - Where &ldquo;green&rdquo; is a lifestyle." /></a>
+		<a href="/our-process/quality-assurance"><img src="<?php echo $template_path; ?>/images/banner-advantage3.jpg" alt="Yes: real people actually pick up the phone." /></a> -->
 	</div>
 	<div class="slidenav"></div>
 </div>
