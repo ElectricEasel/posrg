59e91173a37435d36735a74a7c09cb7076c65749
kylemagnusson1 <kyle@electriceasel.com>
2017-03-16 09:55:24 -0500

    The carwash show

--- modules/mod_welcome/tmpl/default.php
+++ modules/mod_welcome/tmpl/default.php
@@ -20,7 +20,7 @@ $template_path = '/templates/posrg';
 	// ]]>
 	</script>
 	<div class="slides">
-		<a href="/about-us/posrg-news/174-2017-trade-show-schedule"><img src="<?php echo $template_path; ?>/images/midwestfoodserviceexpo.jpg" alt="Midwest Foodservice Expo" /></a>
+		<a href="/about-us/posrg-news/174-2017-trade-show-schedule"><img src="<?php echo $template_path; ?>/images/thecarshow.jpg" alt="The Car Wash Show" /></a>
 		<a href="/about-us/posrg-news/168-posrg-r2-certified-for-responsible-electronics-recycling" style="display: none;"><img src="<?php echo $template_path; ?>/images/banner-responsible-recycling.jpg" alt="POSRG Certified for Responsible Electronics Recycling. Certification positions POSRG as leader in point of sale solutions industry." /></a>
 		<a href="/about-us/posrg-news/166-posrg-naid-aaa-certified-for-data-destruction" style="display: none;"><img src="<?php echo $template_path; ?>/images/banner-naid.jpg" alt="POSRG NAID AAA Certified for Data Destruction" /></a>
 		<a href="/preferred-oems" style="display: none;"><img src="<?php echo $template_path; ?>/images/banner-new-hardware.jpg" alt="New Hardware" /></a>
